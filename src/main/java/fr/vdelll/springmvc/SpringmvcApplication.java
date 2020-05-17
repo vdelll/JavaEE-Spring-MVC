3b389ccd14d6b57ab0a3e2c400416b83dd69839a
Val <valentin.delaunay1@gmail.com>
2020-05-17 19:16:55 +0200

    Création de l'entité patient et de son Repository, accès à H2 database (url localhost:8080/h2-console et jdbc url = jdbc:h2:mem:testdb

--- src/main/java/fr/vdelll/springmvc/SpringmvcApplication.java
+++ src/main/java/fr/vdelll/springmvc/SpringmvcApplication.java
@@ -1,13 +1,34 @@
 package fr.vdelll.springmvc;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringmvcApplication {
+import fr.vdelll.springmvc.dao.PatientRepository;
+import fr.vdelll.springmvc.entities.Patient;
 
+@SpringBootApplication
+public class SpringmvcApplication implements CommandLineRunner {
+
+	@Autowired
+	private PatientRepository patientRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		patientRepository.save(new Patient(null, "Pierre", new Date(), false));
+		patientRepository.save(new Patient(null, "Paul", new Date(), false));
+		patientRepository.save(new Patient(null, "Jacques", new Date(), false));
+		
+		patientRepository.findAll().forEach(p->{
+			System.out.println(p.getName());
+		});
 	}
 
 }
