5b97b180dfc48eb4e473683690159a95774ab059
Val <valentin.delaunay1@gmail.com>
2020-05-18 09:59:12 +0200

    Pagination

--- src/main/java/fr/vdelll/springmvc/web/PatientController.java
+++ src/main/java/fr/vdelll/springmvc/web/PatientController.java
@@ -3,9 +3,12 @@ package fr.vdelll.springmvc.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.vdelll.springmvc.dao.PatientRepository;
 import fr.vdelll.springmvc.entities.Patient;
@@ -22,9 +25,15 @@ public class PatientController {
 	}
 	
 	@GetMapping(path = "/patients")
-	public String list(Model model) {
-		List<Patient> patients = patientRepository.findAll();
-		model.addAttribute("patients", patients);
+	public String list(Model model,
+			@RequestParam(name = "page", defaultValue = "0") int page, 
+			@RequestParam(name = "size", defaultValue = "5") int size) {
+		Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+		
+		model.addAttribute("patients", pagePatients.getContent());
+		model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+		model.addAttribute("currentPage", page);
+		
 		return "patients";
 	}
 
