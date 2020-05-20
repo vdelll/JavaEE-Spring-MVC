8eb89fd1de370bc5d268431b16bcc9dcf1fb46e9
Val <valentin.delaunay1@gmail.com>
2020-05-20 18:15:51 +0200

    JDBC Authentication

--- src/main/java/fr/vdelll/springmvc/security/SecurityConfig.java
+++ src/main/java/fr/vdelll/springmvc/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package fr.vdelll.springmvc.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,45 +13,62 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	// Utilisation de la base de donnée de l'application
+	@Autowired
+	private DataSource dataSource;
+
 	/**
 	 * Permet de dire à spring où aller chercher les utilisateurs
 	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+
 		PasswordEncoder passwordEncoder = passwordEncoder();
-		
+
 		// Définition d'utilisateurs en mémoire
-		// "{noop}mdp" permet de définir qu'on utilise pas d'encodeur (Bcrypt est utilisé par défaut)
-		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("test")).roles("USER");
-		auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("ADMIN", "USER");
+		// "{noop}mdp" permet de définir qu'on utilise pas d'encodeur (Bcrypt est
+		// utilisé par défaut)
+//		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("test")).roles("USER");
+//		auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
+//		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("ADMIN", "USER");
+
+		// Authentification avec JDBC
+		auth.jdbcAuthentication()
+				.dataSource(dataSource)
+				.usersByUsernameQuery("SELECT username AS principal, password AS credentials, active FROM users WHERE username=?")
+				.authoritiesByUsernameQuery("SELECT username AS principal, role AS role FROM users_roles WHERE username=?")
+				.passwordEncoder(passwordEncoder);
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		// Utilisation d'un formulaire d'authentification
 		http.formLogin();
 		// http.httpBasic(); // authentification de http sous forme de pop up
-		
+
 		// Limitation de certaines requêtes
-		http.authorizeRequests().antMatchers("/save**/**", "/delete**/**", "/form**/**").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers("/admin/**", "/save**/**", "/delete**/**", "/form**/**").hasRole("ADMIN");
 		http.authorizeRequests().antMatchers("/patients**/**").hasRole("USER");
-		
+
 		// Toutes les requetes HTTP nécessitent de passer par une authentification
-		// http.authorizeRequests().anyRequest().authenticated(); 
-		
+		// http.authorizeRequests().anyRequest().authenticated();
+
+		// Appliqué par défaut
 		http.csrf();
-		
+
+		// Toutes les ressources sont accessible pour les url contenant /user/**
+		http.authorizeRequests().antMatchers("/user/**").permitAll();
+
 		// Redirige vers la page notAuthorized si erreur 403
 		http.exceptionHandling().accessDeniedPage("/notAuthorized");
-		
+
 	}
-	
+
 	/**
 	 * Dans le contexte d'application
+	 * 
 	 * @return
 	 */
 	@Bean
