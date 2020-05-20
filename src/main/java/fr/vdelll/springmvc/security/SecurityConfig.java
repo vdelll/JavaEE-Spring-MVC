6cc826462c153781e188b25afdce3a995af57475
Val <valentin.delaunay1@gmail.com>
2020-05-20 15:59:14 +0200

    Spring security utilisation de passwordEncoder

--- src/main/java/fr/vdelll/springmvc/security/SecurityConfig.java
+++ src/main/java/fr/vdelll/springmvc/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package fr.vdelll.springmvc.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,11 +17,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		PasswordEncoder passwordEncoder = passwordEncoder();
+		
 		// Définition d'utilisateurs en mémoire
-		// {noop} permet de définir qu'on utilise pas d'encodeur (Bcrypt est utilisé par défaut)
-		auth.inMemoryAuthentication().withUser("user1").password("{noop}test").roles("USER");
-		auth.inMemoryAuthentication().withUser("user2").password("{noop}1234").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN", "USER");
+		// "{noop}mdp" permet de définir qu'on utilise pas d'encodeur (Bcrypt est utilisé par défaut)
+		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("test")).roles("USER");
+		auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("ADMIN", "USER");
 	}
 	
 	@Override
@@ -26,9 +31,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		// Utilisation d'un formulaire d'authentification
 		http.formLogin();
+		// http.httpBasic(); // authentification de http sous forme de pop up
 		
-		// Toutes les requêtes HTTP nécessitent une authentification
-		http.authorizeRequests().anyRequest().authenticated();
+		// Limitation de certaines requêtes
+		http.authorizeRequests().antMatchers("/save**/**", "/delete**/**").hasRole("ADMIN");
+		
+		// Toutes les requetes HTTP nécessitent de passer par une authentification
+		http.authorizeRequests().anyRequest().authenticated(); 
+		
+		http.csrf();
+	}
+	
+	/**
+	 * Dans le contexte d'application
+	 * @return
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
